bfafcb56d08f8722fed2b3a6d4e5b5587f43bf6e
Suellen Karolainy <103966382+SuKarolainy@users.noreply.github.com>
2023-12-07 08:08:28 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿
+decimal peso = 0;
+decimal altura = 0;
+decimal resultado = 0.0m;
+
+Console.WriteLine("Digite sua altura: ");
+Console.WriteLine("Utilize virgula para separar, por exemplo: 1,60");
+altura = Convert.ToDecimal(Console.ReadLine());
+
+Console.WriteLine("Digite seu peso: ");
+peso = Convert.ToDecimal(Console.ReadLine());
+
+resultado = peso / (altura * altura);
+decimal result = decimal.Round(resultado, 2);
+
+Console.WriteLine("Esse é o seu IMC: " + result);
